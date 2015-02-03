81f941da22852084b7daa2835417500c90ecfe42
Joris de Wit <joris.w.dewit@gmail.com>
2015-02-03 11:56:57 -0800

    isolate the scope, attach isOpen, clickInside, & onToggle to scope, fix buggy hide behaviour

--- src/ez-dropdown.js
+++ src/ez-dropdown.js
@@ -1,84 +1,78 @@
 angular.module('ez.dropdown', [])
 
 .controller('DropdownCtrl', ['$scope', '$document', '$compile', function($scope, $document, $compile) {
-  var isShown = false;
   var isCompiled = false;
   var $dropdownEl;
   var $dropdownMenuEl;
-  var toggleFn;
-  var isDisabled = false;
-  var allowClickInside;
   var cloneEl;
 
-  var toggleMenu = function(e, noDigest) {
-    if (isDisabled || allowClickInside && !!cloneEl && !!e && cloneEl.contains(e.target) && e.target.getAttribute('ng-click') !== 'toggleMenu()') {
-      return;
-    }
-
-    if (isShown) {
-      $document.off('click', toggleMenu);
-
-      $dropdownEl.removeClass('open');
-    } else {
-      if (!isCompiled) {
-        isCompiled = true;
-
-        // must clone content so it does not get overwritten
-        $compile($dropdownMenuEl)($scope, function(clone) {
-          cloneEl = clone[0];
-          $dropdownEl.append(clone);
-        });
-      }
-
-      // allow first click to happen
+  var hideFn = function(e) {
+    if (!!e && !!$scope.clickInside && !!cloneEl && cloneEl.contains(e.target)) {
       setTimeout(function() {
-        $document.on('click', toggleMenu);
+        $document.one('click', hideFn);
       });
-
-      $dropdownEl.addClass('open');
-    }
-
-    isShown = !isShown;
-
-    if (typeof toggleFn === 'function') {
-      toggleFn(isShown);
-    }
-
-    if (!noDigest) {
+    } else {
+      $scope.isOpen = false;
       $scope.$apply();
     }
   };
 
-  this.toggleMenu = toggleMenu;
+  var show = function() {
+    if (!isCompiled) {
+      isCompiled = true;
 
-  this.init = function(el, dropdownMenuEl, attrs) {
-    $dropdownEl = el;
-    $dropdownMenuEl = dropdownMenuEl.clone();
-    toggleFn = attrs.onToggle !== null ? $scope.$eval(attrs.onToggle) : null;
-    allowClickInside = attrs.hasOwnProperty('clickInside');
-
-    if (!!attrs.isOpen) {
-      $scope.$watch(attrs.isOpen, function(newVal, oldVal) {
-        if (newVal === oldVal) {
-         return;
-        }
-
-        if (newVal !== isShown) {
-          toggleMenu(null, true);
-          isDisabled = true;
-
-          // disable click from re-toggling dropdown
-          setTimeout(function() {
-            isDisabled = false;
-          }, 100);
-        }
+      // must clone content so it does not get overwritten
+      $compile($dropdownMenuEl)($scope.$parent, function(clone) {
+        cloneEl = clone[0];
+        $dropdownEl.append(clone);
       });
+    }
+
+    // allow first click to happen
+    setTimeout(function() {
+      $document.one('click', hideFn);
+    });
+
+    $dropdownEl.addClass('open');
+
+    if (typeof $scope.onToggle === 'function') {
+      $scope.onToggle(true);
     }
   };
 
+  var hide = function() {
+    $dropdownEl.removeClass('open');
+
+    if (typeof $scope.onToggle === 'function') {
+      $scope.onToggle(false);
+    }
+
+    $document.off('click', hideFn);
+  };
+
+  this.init = function(el, dropdownMenuEl) {
+    $dropdownEl = el;
+    $dropdownMenuEl = dropdownMenuEl.clone();
+
+    $scope.$watch('isOpen', function(newVal, oldVal) {
+      if (newVal === oldVal) {
+       return;
+      }
+
+      if (!!newVal) {
+        show();
+      } else {
+        hide();
+      }
+    });
+  };
+
+  this.toggle = function() {
+    $scope.isOpen = !$scope.isOpen;
+  };
+
   $scope.$on('$destroy', function() {
-    $document.off('click', toggleMenu);
-    $dropdownEl = $dropdownMenuEl = null;
+    $dropdownEl = $dropdownMenuEl = cloneEl = null;
   });
 }])
 
@@ -86,11 +80,16 @@ angular.module('ez.dropdown', [])
   return {
     restrict: 'C',
     controller: 'DropdownCtrl',
-    compile: function(el, attrs) {
+    scope: {
+      isOpen: '=?',
+      clickInside: '=?',
+      onToggle: '=?',
+    },
+    compile: function(el) {
       var $dropdownMenuEl = el.find('.dropdown-menu').remove();
 
       return function(scope, el, attrs, ctrl) {
-        ctrl.init(el, $dropdownMenuEl, attrs);
+        ctrl.init(el, $dropdownMenuEl);
       };
     }
   };
@@ -101,10 +100,9 @@ angular.module('ez.dropdown', [])
     restrict: 'C',
     require: '^dropdown',
     link: function(scope, el, attrs, ctrl) {
-      el.on('click', ctrl.toggleMenu);
-
-      scope.$on('$destroy', function() {
-        el.off('click', ctrl.toggle);
+      el.on('click', function(e) {
+        ctrl.toggle();
+        scope.$apply();
       });
     }
   };
